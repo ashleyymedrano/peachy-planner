b9fe3f3668dce564e83307e19be5911054f2410f
Ashley <amedrano19@cnm.edu>
2024-10-08 22:43:41 -0600

    started on home page, added designs.

--- src/page.jsx
+++ src/page.jsx
@@ -1,11 +1,35 @@
 import './App.css'
+import title from '/src/assets/title.png'
+
 
 function Page() {
-  return (
-    <>
-      <p>hellur world</p>
-    </>
-  )
+    return (
+        <>
+            <div className="container">
+                <div className="title-container">
+                    <img src={title} alt="Peach" height="300" width="400" className="title"/>
+                    <h1 className="syp">sweeten your productivity</h1>
+                </div>
+                <div className="card">
+                    <input type="text"
+                           placeholder="email"
+                           className="email"/>
+                    <input type="password"
+                           placeholder="password"
+                       className="password"/>
+                <label className="remember-me">
+                    <input type="checkbox"
+                           className="checkbox"/> Remember me
+                </label>
+                <button className="login-button">log in</button>
+                <a href="/" className="forgot-password">forgot password?</a>
+                <div>
+                <button className="new-user-button">not a user? create an account.</button>
+                </div>
+            </div>
+            </div>
+        </>
+    );
 }
 
-export default Page
+export default Page;
