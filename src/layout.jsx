111c030c7674497db281552d4701e1412e4692bb
Ashley <amedrano19@cnm.edu>
2024-10-09 18:59:17 -0600

    Added route path

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Page from './page.jsx'
+import Loading from './loading/page.jsx'
 import './globals.css'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Page />
-  </StrictMode>,
+    <StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<Page />} />
+                <Route path="/loading" element={<Loading />} />
+            </Routes>
+        </Router>
+    </StrictMode>,
 )
