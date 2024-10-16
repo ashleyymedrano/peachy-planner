d1c62394f520e86d1d5d5f55259bc510ff658d7f
Ashley <amedrano19@cnm.edu>
2024-10-15 22:05:32 -0600

    added routing for tasks page

--- src/layout.jsx
+++ src/layout.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Page from './page.jsx'
 import Loading from './loading/page.jsx'
+import Tasks from './tasks/page.jsx'
 import './globals.css'
 
 createRoot(document.getElementById('root')).render(
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')).render(
             <Routes>
                 <Route path="/" element={<Page />} />
                 <Route path="/loading" element={<Loading />} />
+                <Route path="/tasks" element={<Tasks />} />
             </Routes>
         </Router>
     </StrictMode>,
